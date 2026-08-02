5cd6cc1d06ddd7f4a09db077418cbe71dfc4fe2e
Minach <ignaciomedinar@gmail.com>
2026-08-02 16:13:41 +0200

    for ads

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,17 @@ import type { NextConfig } from 'next';
 
 const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'goal-genius.net' }],
+        destination: 'https://www.goal-genius.net/:path*',
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default withNextIntl(nextConfig);
